4e9e883127adbf0a0c6d77e4a91edb3f1b3a8b11
nildoneumann <leonildoneumann@gmail.com>
2026-04-20 17:19:23 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "radio-app-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
+});
