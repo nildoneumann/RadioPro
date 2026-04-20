025913777af2e58eeffac55fb4b1649bf95dabc6
nildoneumann <leonildoneumann@gmail.com>
2026-04-20 18:29:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,10 @@ self.addEventListener("install", e=>{
       return cache.addAll([
         "/",
         "/index.html"
+        "./manifest.json",
+        "./service-worker.js",
+        "./icon-192.png",
+       "./icon-512.png"
       ]);
     })
   );
