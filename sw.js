94f7dc2f6a26d6e65b98840dc4746d7fa262f449
nildoneumann <leonildoneumann@gmail.com>
2026-05-27 09:05:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,48 @@
-const CACHE = "radio-app-v1";
+const CACHE = "radio-app-v2";
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(cache=>{
-      return cache.addAll([
-        "/",
-        "/index.html"
-        "./manifest.json",
-        "./service-worker.js",
-        "./icon-192.png",
-       "./icon-512.png"
-      ]);
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
+
+  // ativa imediatamente
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
+// ATIVAÇÃO
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
